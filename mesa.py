0503b8fafa07fd626b517edff92aadc64049b9f9
Thiago Santos <deoliveirasantosthiago4@gmail.com>
2021-11-04 21:08:18 -0300

    Criacao de outros menus.

--- mesa.py
+++ mesa.py
@@ -4,6 +4,10 @@ from flask import Flask, render_template
 app = Flask(__name__, template_folder='template')
 
 @app.route('/')
+def login():
+    return render_template('login.html')
+
+@app.route('/index.html')
 def index():
     return render_template('index.html')
 
